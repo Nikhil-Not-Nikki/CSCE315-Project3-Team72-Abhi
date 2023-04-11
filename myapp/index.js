5e6c56463a803620163f42a241d9e1dd6d4266c5
Cameron <mendoza.cameron21@gmail.com>
2023-04-10 19:14:07 -0500

    creating parse function for inventory changes

--- myapp/index.js
+++ myapp/index.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv').config()
 //express app functionality
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 
 // Create pool
@@ -15,7 +15,8 @@ const pool = new Pool({
     database: process.env.PSQL_DATABASE,
     password: process.env.PSQL_PASSWORD,
     port: process.env.PSQL_PORT,
-    ssl: {rejectUnauthorized: false}
+    ssl: {rejectUnauthorized: false},
+    headers: {'Access-Control-Allow-Origin': '*'}
 });
 
 
@@ -27,13 +28,23 @@ process.on('SIGINT', function() {
 });
 
 //setting view
-app.set("view engine", "ejs");
+//app.set("view engine", "ejs");
 
 //default route
-app.get('/', (req, res) => {
-    const data = {name: 'Cameron'};
-    res.render('index', data);
+// app.get('/', (req, res) => {
+//     const data = {name: 'Cameron'};
+//     res.render('index', data);
+// });
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
 });
+
 
 //different route: add /user to the end of the link
 app.get('/user', (req,res) =>{
@@ -48,11 +59,46 @@ app.get('/user', (req,res) =>{
 
         const data = {menu: menu};
         console.log(menu);
-        res.render('user', data);
+        res.json({message: menu});
     });
 });
+
+app.get('/inventory', (req,res) =>{
+    inventory = []
+    pool
+        .query('SELECT * FROM inventory;')
+
+        .then(query_res => {
+            for (let i = 0; i < query_res.rowCount; i++){
+            menu.push(query_res.rows[i]);
+            }
+
+        const data = {inventory: inventory};
+        console.log(inventory);
+        res.json({message: inventory});
+    });
+
+ 
+
+
+});
+
+
 
 //console to know what the link is
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
+
+
+function parse_transaction(str)
+{
+    const amount = [];
+    const index = str.indexOf("|");
+    const nextChar = str.charAt(index + 1);
+    //parse the transaction line of what is being ordered
+    amount[0] = parseInt(str, charAt(0));
+    amount[1] = parseInt(nextChar);
+    
+    //update the inventory based on the count
+}
